04e7b38e9a3b0f2c8603b3ad88dd55db7fde43dd
JulianSauer <julian_sauer@gmx.net>
2017-04-20 13:31:26 +0200

    Removed exceptions from main method.

--- src/main/java/de/juliansauer/Main.java
+++ src/main/java/de/juliansauer/Main.java
@@ -1,21 +1,22 @@
 package de.juliansauer;
 
-import com.tinkerforge.AlreadyConnectedException;
-import com.tinkerforge.NotConnectedException;
-import com.tinkerforge.TimeoutException;
 import de.juliansauer.coffee.CoffeeScale;
 
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, AlreadyConnectedException, TimeoutException, NotConnectedException, InterruptedException {
+    public static void main(String[] args) {
 
         CoffeeScale scale = new CoffeeScale("B12", "localhost", 4223);
         scale.startListening();
 
         System.out.println("Press enter to exit");
-        System.in.read();
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         scale.closeConnection();
 
     }
